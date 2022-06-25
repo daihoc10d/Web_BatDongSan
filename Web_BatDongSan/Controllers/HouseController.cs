52c5014a9d38e7f80dde39cd0777508a0790d88b
phungdai.hoc@gmail.com <phungdai.hoc@gmail.com>
2022-06-25 16:25:01 +0700

    update

--- Web_BatDongSan/Controllers/HouseController.cs
+++ Web_BatDongSan/Controllers/HouseController.cs
@@ -10,24 +10,130 @@ namespace Web_BatDongSan.Controllers
     public class HouseController : Controller
     {
         // GET: House
-        BDSContext db =new BDSContext();
-        public ActionResult Index()
+        
+        public ActionResult ListHouse()
+        {
+            BDSContext context = new BDSContext();
+
+            var ListHouse = context.Houses.ToList();
+            return View(ListHouse);
+        }
+        public ActionResult Detail(int id)
+        {
+            BDSContext context = new BDSContext();
+            var D_theloai = context.Houses.Where(m => m.IDHouse == id).First();
+            return View(D_theloai);
+        }
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            BDSContext context = new BDSContext();
+            House house = context.Houses.SingleOrDefault(p => p.IDHouse == id);
+            if (house == null)
+            {
+                return HttpNotFound();
+            }
+            return View(house);
+        }
+
+        public ActionResult Create()
         {
             return View();
         }
-        public ActionResult TopHouse(bool? IsBan)
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, House b)
         {
-            var lst = db.Houses.Where(q => q.State != MState.DELETE).Take(3 * 1).ToList();
-            foreach (var item in lst)
-            {
-                item.xImage = item.ImagesHouses.FirstOrDefault(q => q.IDHouse == item.IDHouse && q.Rank == 0);
-                //item.xAddress1 = @item.AddressHouses.FirstOrDefault(q => q.Address.Rank == 1)?.Address;
-                //item.xAddress2 = @item.AddressHouses.FirstOrDefault(q => q.Address.Rank == 2)?.Address;
-                //item.xAddress3 = @item.AddressHouses.FirstOrDefault(q => q.Address.Rank == 3)?.Address;
-            }
+            BDSContext context = new BDSContext();
+            var E_id = Convert.ToInt32(collection["IDHouse"]);
+            var E_Code = collection["Code"];
+            var E_Name = collection["Name"];
+            var E_Sumary = collection["Sumary"];
+            var E_BedRoom = collection["BedRoom"];
 
-            ViewBag.IsBan = IsBan;
-            return View(lst);
+            var E_Area = collection["Area"];
+            var E_Price = Convert.ToInt32(collection["Price"]);
+            var E_State = collection["State"];
+            
+            if (string.IsNullOrEmpty(E_Code))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                decimal a = 0;
+                try
+                {
+                    a = Convert.ToDecimal(E_Price.ToString());
+                }
+                catch
+                {
+                    a = 0;
+                }
+
+                b.IDHouse = E_id;
+                b.Code = E_Code.ToString();
+                b.Name = E_Name.ToString();
+                b.Sumary = E_Sumary.ToString();
+                b.Price = a;
+                context.Houses.Add(b);
+                context.SaveChanges();
+            }
+            return RedirectToAction("listHouse");
         }
+
+        //public ActionResult Edit(int id)
+        //{
+        //    Modelcontext context = new Modelcontext();
+        //    var E_category = context.Book1.First(m => m.ID == id);
+        //    return View(E_category);
+        //}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, FormCollection collection, Book1 b)
+        //{
+        //    Modelcontext context = new Modelcontext();
+        //    var E_sach = context.Book1.FirstOrDefault(m => m.ID == id);
+        //    var E_id = Convert.ToInt32(collection["ID"]);
+        //    var E_title = collection["Title"];
+        //    var E_description = collection["Description"];
+        //    var E_author = collection["Author"];
+        //    var E_images = collection["Images"];
+        //    var E_price = Convert.ToInt32(collection["Price"]);
+        //    if (string.IsNullOrEmpty(E_title))
+        //    {
+        //        ViewData["Error"] = "Don't empty!";
+        //    }
+        //    else
+        //    {
+        //        b.ID = E_id;
+        //        b.Title = E_title.ToString();
+        //        b.Description = E_description.ToString();
+        //        b.Author = E_author.ToString();
+        //        b.Images = E_images.ToString();
+        //        b.Price = E_price.ToString();
+        //        context.Book1.AddOrUpdate(b);
+        //        context.SaveChanges();
+        //    }
+        //    return RedirectToAction("ListBook");
+        //}
+        //public ActionResult Delete(int id)
+        //{
+        //    Modelcontext context = new Modelcontext();
+        //    var D_sach = context.Book1.First(m => m.ID == id);
+        //    return View(D_sach);
+        //}
+
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    Modelcontext context = new Modelcontext();
+        //    Book1 dbDelete = context.Book1.FirstOrDefault(p => p.ID == id);
+        //    if (dbDelete != null)
+        //    {
+        //        context.Book1.Remove(dbDelete);
+        //        context.SaveChanges();
+        //    }
+        //    return RedirectToAction("ListBook");
+        //}
     }
 }
